1c6da3dafc62b5a4eef32e63b6eaa77d6c48df8d
ToiMaiTaiMoi_Tum <iloveusomuchx2@gmail.com>
2025-02-21 14:41:26 +0700

    "lab finished"

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,110 @@
 #include <windows.h>
-
+#include<string>
+#include<cstdio>
+HWND textfield , button, Firstint,SecondInt;
+char textSaved1[20],textSaved2[20];
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
+				case WM_CREATE:
+					textfield = CreateWindow("STATIC","Please input two numbers",
+					WS_VISIBLE | WS_CHILD,
+				20,20,180,25,hwnd,NULL,NULL,NULL);
+					
+				CreateWindow("BUTTON","+",
+					WS_VISIBLE | WS_CHILD,
+				30,120,25,25,hwnd,(HMENU) 1,NULL,NULL);
+				CreateWindow("BUTTON","-",
+					WS_VISIBLE | WS_CHILD,
+				60,120,25,25,hwnd,(HMENU) 2,NULL,NULL);
+				CreateWindow("BUTTON","*",
+					WS_VISIBLE | WS_CHILD,
+				90,120,25,25,hwnd,(HMENU) 3,NULL,NULL);
+				CreateWindow("BUTTON","/",
+					WS_VISIBLE | WS_CHILD,
+				120,120,25,25,hwnd,(HMENU) 4,NULL,NULL);
+
+
+				Firstint = CreateWindow("EDIT","",
+					WS_BORDER|WS_VISIBLE | WS_CHILD,
+				30,50,150,20,hwnd,NULL,NULL,NULL);
+				SecondInt= CreateWindow("EDIT","",
+					WS_BORDER|WS_VISIBLE | WS_CHILD,
+				30,80,150,20,hwnd,NULL,NULL,NULL);
+					
+				
+
+				break;
+
+
+		case WM_COMMAND:
+				switch(LOWORD(wParam)){
+					
+					case 1:{
+					float firstint = 0,secondint = 0;
+					firstint = GetWindowText(Firstint,&textSaved1[0],20);
+					secondint= GetWindowText(SecondInt,&textSaved2[0],20);
+					float num1 = std::stof(textSaved1);
+					float num2 = std::stof(textSaved2);
+					num1 = num1 + num2;
+					char str[20];
+					sprintf(str, "%f", num1);
+			
+
+					::MessageBox(hwnd,str,"Result",MB_OK);
+					break;
+					}
+					case 2:{
+						float firstint = 0,secondint = 0;
+					firstint = GetWindowText(Firstint,&textSaved1[0],20);
+					secondint= GetWindowText(SecondInt,&textSaved2[0],20);
+					float num1 = std::stof(textSaved1);
+					float num2 = std::stof(textSaved2);
+					num1 = num1 - num2;
+					char str[20];
+					sprintf(str, "%f", num1);
+					::MessageBox(hwnd,str,"Result",MB_OK);
+
+
+
+					}
+					break;
+					case 3:{
+						float firstint = 0,secondint = 0;
+					firstint = GetWindowText(Firstint,&textSaved1[0],20);
+					secondint= GetWindowText(SecondInt,&textSaved2[0],20);
+					float num1 = std::stof(textSaved1);
+					float num2 = std::stof(textSaved2);
+					num1 = num1 * num2;
+					char str[20];
+					sprintf(str, "%f", num1);
+					::MessageBox(hwnd,str,"Result",MB_OK);
+
+
+
+					}
+
+					break;
+
+					case 4:{
+						float firstint = 0,secondint = 0;
+					firstint = GetWindowText(Firstint,&textSaved1[0],20);
+					secondint= GetWindowText(SecondInt,&textSaved2[0],20);
+					float num1 = std::stof(textSaved1);
+					float num2 = std::stof(textSaved2);
+					num1 = num1 / num2;
+					char str[20];
+					sprintf(str, "%f", num1);
+					::MessageBox(hwnd,str,"Result",MB_OK);
+
+
+
+					}
+				}
+
 		
+				break;
+
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
 			PostQuitMessage(0);
@@ -31,7 +132,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = CreateSolidBrush(RGB(40, 50, 200)); 
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,12 +142,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, /* x */
-		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
-		NULL,NULL,hInstance,NULL);
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_OVERLAPPED  | WS_SYSMENU ,
+		100, /* x */
+		100, /* y */
+		250, /* width */
+		200, /* height */
+		NULL,NULL,NULL,NULL);
 
 	if(hwnd == NULL) {
 		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
